36920a3e9bab389be3ddd04b7744309905325fc1
Dinesh <dinesh.majeti@gmail.com>
2016-10-30 09:22:56 -0500

    query for all children of parent

---
+++ php/allChildForParent.php
@@ -0,0 +1,37 @@
+<?php
+
+$enteredUser = $_GET["username"];
+
+//Variables for connecting to your database.
+//These variable values come from your hosting account.
+$hostname = "codered1.db.11273437.hostedresource.com";
+$username = "codered1";
+$dbname = "codered1";
+
+//These variable values need to be changed by you before deploying
+$password = "Legends1!";
+$usertable = "users";
+$yourfield = "username";
+
+//Connecting to your database
+$conn = mysqli_connect($hostname, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+//Fetching from your database table.
+$query = "SELECT * FROM $usertable WHERE parent = '$enteredUser'";
+$result = mysqli_query($conn, $query);
+
+
+$results = array();
+if ($result) {
+    while($row = mysqli_fetch_assoc($result)) {
+        $results[] = $row;
+    }
+}
+
+echo json_encode($results)
+?>
